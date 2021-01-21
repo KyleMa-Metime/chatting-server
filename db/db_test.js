a455f6dcdc704e489708912ba08492ba0a86d661
forest <273777415@qq.com>
2021-01-21 16:13:01 +0800

    last version

--- db/db_test.js
+++ db/db_test.js
@@ -1,57 +1,82 @@
-
-const md5 = require("blueimp-md5")
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("数据库连接成功")
-});
-
-const userSchema = mongoose.Schema({
-    username:{type:String , require:true},
-    password:{type:String , require:true},
-    gender:{type:String , require:true},
-    avatar:{type:String}
+/*
+测试使用mongoose操作mongodb数据库
+1. 连接数据库
+  1.1. 引入mongoose
+  1.2. 连接指定数据库(URL只有数据库是变化的)
+  1.3. 获取连接对象
+  1.4. 绑定连接完成的监听(用来提示连接成功)
+2. 得到对应特定集合的Model
+  2.1. 字义Schema(描述文档结构)
+  2.2. 定义Model(与集合对应, 可以操作集合)
+3. 通过Model或其实例对集合数据进行CRUD操作
+  3.1. 通过Model实例的save()添加数据
+  3.2. 通过Model的find()/findOne()查询多个或一个数据
+  3.3. 通过Model的findByIdAndUpdate()更新某个数据
+  3.4. 通过Model的remove()删除匹配的数据
+ */
+const md5 = require('blueimp-md5') // md5加密的函数
+/*1. 连接数据库*/
+// 1.1. 引入mongoose
+const mongoose = require('mongoose')
+// 1.2. 连接指定数据库(URL只有数据库是变化的)
+mongoose.connect('mongodb://localhost:27017/gzhipin_test')
+// 1.3. 获取连接对象
+const conn = mongoose.connection
+// 1.4. 绑定连接完成的监听(用来提示连接成功)
+conn.on('connected', function () { // 连接成功回调
+  console.log('数据库连接成功, YE!!!')
 })
 
-const UserModel = mongoose.model("user",userSchema)
+/*2. 得到对应特定集合的Model*/
+// 2.1. 字义Schema(描述文档结构)
+const userSchema = mongoose.Schema({ // 指定文档的结构: 属性名/属性值的类型, 是否是必须的, 默认值
+  username: {type: String, required: true}, // 用户名
+  password: {type: String, required: true}, // 密码
+  type: {type: String, required: true}, // 用户类型: dashen/laoban
+  header: {type: String}
+})
 
-function testSave(){
-    const userModel = new UserModel({username:'dundun',password:md5('123'),gender:'male'})
-    userModel.save(function(err,doc){
-        console.log('打印看看',err,doc)
-    })
+// 2.2. 定义Model(与集合对应, 可以操作集合)
+const UserModel = mongoose.model('user', userSchema) // 集合的名称为: users
+
+/*3. 通过Model或其实例对集合数据进行CRUD操作*/
+// 3.1. 通过Model实例的save()添加数据
+function testSave() {
+  // 创建UserModel的实例
+  const userModel = new UserModel({username: 'Bob', password: md5('234'), type: 'laoban'})
+  // 调用save()保存
+  userModel.save(function (error, user) {
+    console.log('save()', error, user)
+  })
 }
-
 // testSave()
-function testFind(){
-    //返回全部，数组类型
-    UserModel.find(function(err,users){
-        console.log('find',err,users)
-    })
-    //条件查询，对象类型
-    UserModel.findOne({_id:'60017266b35ed63e3c003063'},function(err,user){
-        console.log('findOne',err,user)
 
-    })
+// 3.2. 通过Model的find()/findOne()查询多个或一个数据
+function testFind() {
+  // 查询多个: 得到是包含所有匹配文档对象的数组, 如果没有匹配的就是[]
+  UserModel.find({_id:'5ae3d7b7614c613db8abb914'}, function (error, users) {
+    console.log('find()', error, users)
+  })
+  // 查询一个: 得到是匹配的文档对象, 如果没有匹配的就是null
+  UserModel.findOne({_id:'5ae3d7b7614c613db8abb914'}, function (error, user) {
+    console.log('findOne()', error, user)
+  })
 }
-
 // testFind()
 
-function testUpdate(){
-    UserModel.findByIdAndUpdate({_id:'60017266b35ed63e3c003063'},{username:'hahaha'},function(err,oldUser){
-        console.log('findByIdAndUpdate',err,oldUser)
-    })
+// 3.3. 通过Model的findByIdAndUpdate()更新某个数据
+function testUpdate() {
+  UserModel.findByIdAndUpdate({_id:'5ae3d7b7614c613db8abb914'},
+    {username:'Jack'}, function (error, oldUser) {
+    console.log('findByIdAndUpdate()', error, oldUser)
+  })
 }
 // testUpdate()
 
-function testDelete(){
-    // UserModel.findByIdAndRemove
-    UserModel.findByIdAndDelete({_id:'60017266b35ed63e3c003063'},function(err,deleteUser){
-        console.log('findByIdAndDelete',err,deleteUser)
-    })
+// 3.4. 通过Model的remove()删除匹配的数据
+function testDelete() {
+  UserModel.remove({_id:'5ae3d7b7614c613db8abb914'}, function (error, doc) {
+    console.log('remove()', error, doc) // {n: 1/0, ok: 1}
+  })
 }
-
 testDelete()
